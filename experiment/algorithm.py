51ee85460657ed92f6879b9d4dcc36e5b761d589
Inf1n1te <t.kerkhoven@student.utwente.nl>
2016-03-29 19:04:47 +0200

    test stuff

--- experiment/algorithm.py
+++ experiment/algorithm.py
@@ -44,41 +44,40 @@ def new_color(g: Graph):
 
 def color_refine(g: Graph):
     # color_dict{key, value} = {colornum, (degree, nbs_dict)}
-    if hasattr(g, "color_dict"):
-        changed = True
-        while changed:
-            changed = False
-            for v in g.vertices():
-                nbs_dict = create_nbs_dict(v)
-                if g.color_dict[v.colornum][1] == nbs_dict:
-                    # dont update, as the nbs are equivalent
-                    pass
-                else:
-                    existing_config = False
-                    for c, (d, n) in g.color_dict.items():
-                        if d == v.degree() and n == nbs_dict:
-                            # already assigned this configuration a new color
-                            v.new_colornum = c
-                            existing_config = True
-                            break
-                    if not existing_config:
-                        # This configuration is not yet known, assign a new color
-                        c = new_color(g)
+    # if not hasattr(g, "color_dict"):
+    initial_coloring(g)
+    changed = True
+    while changed:
+        changed = False
+        for v in g.vertices():
+            nbs_dict = create_nbs_dict(v)
+            if g.color_dict[v.colornum][1] == nbs_dict:
+                # dont update, as the nbs are equivalent
+                pass
+            else:
+                existing_config = False
+                for c, (d, n) in g.color_dict.items():
+                    if d == v.degree() and n == nbs_dict:
+                        # already assigned this configuration a new color
                         v.new_colornum = c
-                        g.color_dict[c] = (v.degree(), nbs_dict)
+                        existing_config = True
+                        break
+                if not existing_config:
+                    # This configuration is not yet known, assign a new color
+                    c = new_color(g)
+                    v.new_colornum = c
+                    g.color_dict[c] = (v.degree(), nbs_dict)
 
-            for v in g.vertices():
-                # update colors
-                if hasattr(v, 'new_colornum'):
-                    v.colornum = v.new_colornum
-                    del v.new_colornum
-                    changed = True
-                # update color_dict
-                g.color_dict[v.colornum] = (v.degree(), create_nbs_dict(v, True))
+        for v in g.vertices():
+            # update colors
+            if hasattr(v, 'new_colornum'):
+                v.colornum = v.new_colornum
+                del v.new_colornum
+                changed = True
+            # update color_dict
+            g.color_dict[v.colornum] = (v.degree(), create_nbs_dict(v, True))
 
-        return
-    else:
-        raise GraphError("Graph has no color dictionary")
+    return
 
 
 def branching(g: Graph):
---
+++ experiment/test.py
@@ -0,0 +1,81 @@
+from timeit import *
+
+from experiment.algorithm import color_refine
+from experiment.graphIO import loadgraph
+
+threepaths = []
+
+
+def main():
+    path = '../graphs/threepaths'
+    threepaths.append(loadgraph(path + '5.gr'))
+    threepaths.append(loadgraph(path + '10.gr'))
+    threepaths.append(loadgraph(path + '20.gr'))
+    threepaths.append(loadgraph(path + '40.gr'))
+    threepaths.append(loadgraph(path + '80.gr'))
+    threepaths.append(loadgraph(path + '160.gr'))
+    threepaths.append(loadgraph(path + '320.gr'))
+    threepaths.append(loadgraph(path + '640.gr'))
+    threepaths.append(loadgraph(path + '1280.gr'))
+    threepaths.append(loadgraph(path + '2560.gr'))
+    threepaths.append(loadgraph(path + '5120.gr'))
+    threepaths.append(loadgraph(path + '10240.gr'))
+    print(min(Timer(t0).repeat(3, 1)))
+    print(min(Timer(t1).repeat(3, 1)))
+    print(min(Timer(t2).repeat(3, 1)))
+    print(min(Timer(t3).repeat(3, 1)))
+    print(min(Timer(t4).repeat(3, 1)))
+    print(min(Timer(t5).repeat(3, 1)))
+    print(min(Timer(t6).repeat(3, 1)))
+    print(min(Timer(t7).repeat(3, 1)))
+
+
+def t0():
+    color_refine(threepaths[0])
+
+
+def t1():
+    color_refine(threepaths[1])
+
+
+def t2():
+    color_refine(threepaths[2])
+
+
+def t3():
+    color_refine(threepaths[3])
+
+
+def t4():
+    color_refine(threepaths[4])
+
+
+def t5():
+    color_refine(threepaths[5])
+
+
+def t6():
+    color_refine(threepaths[6])
+
+
+def t7():
+    color_refine(threepaths[7])
+
+
+def t8():
+    color_refine(threepaths[8])
+
+
+def t9():
+    color_refine(threepaths[9])
+
+
+def t10():
+    color_refine(threepaths[10])
+
+
+def t11():
+    color_refine(threepaths[11])
+
+
+main()
